c7b2a5b2547626041dca47161c3d005f44fb23c3
John Zhang <john.shimin.zhang@gmail.com>
2014-09-24 21:14:11 -0400

    skeleton out rspec for game file

---
+++ spec/game_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'game'
+
+describe Game do 
+
+  it "should be created with players, a deck, and an empty pot"
+  
+  it "should deal players in"
+  
+  it "should ask players to discard"
+  
+  it "should ask players if they would like to bet, see, or raise"
+  
+  it "should show player pot amount"
+  
+  it "should keep track of pot amount"
+  
+  it "should keep track of players"
+  
+  it "should finish betting and declare winner"
+  
+  it "should pay out the pot to winners"
+  
+  it "should comment on players with no bankrolls and hail them a cab back"
+  
+  
+end
